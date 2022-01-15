e3a45b7f9d396b86e5a45fe8f3069cca6858e969
whtjdals016 <whtjdals016@hanmail.net>
2022-01-15 14:57:15 +0800

    refactor

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ import RecipeReviewCard from "./presentation/component/post";
 function App() {
   const testOpenSea = async () => {
     let server = new ServerDataSource();
-    let res = await server.getAccountOpenSea(
+    let res1 = await server.getAccountOpenSea(
       "0xD59134a3E004609f39C601cE6F0739df2cc78240"
     );
-    console.log(res);
+    let res2 = await server.getPriceCovalent(
+      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    );
+    console.log(res1);
+    console.log(res2);
     console.log(process.env.REACT_APP_TEST_KEY);
     console.log(process.env.NODE_ENV);
   };
