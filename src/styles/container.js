029345e3323d4e8b5b18c140d53095fcba653e7c
Chanhyeok Yim <chanhyeok.yim@gmail.com>
2022-01-15 15:45:46 +0800

    fix: height issue

--- src/styles/container.js
+++ src/styles/container.js
@@ -4,11 +4,11 @@ export const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+  width: 100%;
 `;
 
 export const MainHeroContainer = styled.div`
   display: flex;
   flex-direction: row;
-  height: 90.5%;
-  width: 100%;
+  height: 92%;
 `;
