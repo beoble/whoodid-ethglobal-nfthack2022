6dd53993fb444b8012ebced72bb4654c19a40091
Jungwoo Yun <bamnenim@gmail.com>
2022-01-16 05:34:21 +0900

    connect wallet profile

--- src/presentation/component/profileDrawer/profileDrawer.js
+++ src/presentation/component/profileDrawer/profileDrawer.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
-import {MaterialProfileDrawer} from "./profileDrawer.style";
+import { MaterialProfileDrawer } from "./profileDrawer.style";
+import useWallet from "../../../hook/useWallet";
 
 const ProfileDrawer = ({ isProfileDrawerOpen, setIsProfileDrawer }) => {
+  const { connected } = useWallet();
   return (
-      <MaterialProfileDrawer open={isProfileDrawerOpen} anchor="right" onClose={() => setIsProfileDrawer(false)}>
-        fuck you sex
-      </MaterialProfileDrawer>
+    <MaterialProfileDrawer
+      open={isProfileDrawerOpen}
+      anchor="right"
+      onClose={() => setIsProfileDrawer(false)}
+    >
+      {connected ? <div></div> : "Please Connect to wallet"}
+    </MaterialProfileDrawer>
   );
 };
 
