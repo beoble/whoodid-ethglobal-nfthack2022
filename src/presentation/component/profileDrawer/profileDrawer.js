bd2377c43d7dec41d505e0a2f57cc5cd5601a73b
Chanhyeok Yim <chanhyeok.yim@gmail.com>
2022-01-16 06:21:04 +0800

    fix: profile drawer

--- src/presentation/component/profileDrawer/profileDrawer.js
+++ src/presentation/component/profileDrawer/profileDrawer.js
@@ -3,42 +3,51 @@ import useWallet from "../../../hook/useWallet";
 import {
   MainNftProfileImage,
   MaterialProfileDrawer,
+  OwnedNftImageList,
   ProfileDrawerInnerContainer,
 } from "./profileDrawer.style";
 import NFTProfileSampleImage from "../../../assets/nft/hood512.png";
 import Typography from "@mui/material/Typography";
 import OpenSeaVerified from "../../../assets/opensea_verified.png";
+import { ImageListItem } from "@mui/material";
 
 const ProfileDrawer = ({
   isProfileDrawerOpen,
-  setIsProfileDrawer,
+  setIsProfileDrawerOpen,
   currentNftProfile,
+  setCurrentNftProfile,
   name,
   isVerified,
   nftCollection,
+  followingHashtags,
 }) => {
   // Mock
   name = "Kryptonium.eth";
   isVerified = true;
-  nftCollection = [];
-  const { connected } = useWallet();
+  nftCollection = [
+    "https://lh3.googleusercontent.com/Qls8qYF7Gbgev3ceQXo5wJ_sShclxl7MwWkpmQ_aRBC3qBLjawZJ17_GT45GzO1kUEiqRpOTUrPR4sLF-f0I9IiUywuqEkM5JAP5",
+    "https://lh3.googleusercontent.com/fLMivNtc1uMXfaC437UBu0EWxXqdg4nA8jBXQ9tv4ieTjbJJ8jPgZDsddW2U8LvdmJpFPU7PF3ljk1lvtFzwUDpHpjpVx8AEMvzY",
+  ];
+  let mockConnected = true;
+  const { connected, accounts } = useWallet();
 
   return (
     <MaterialProfileDrawer
-      open={isProfileDrawerOpen}
+      open={true}
       anchor="right"
-      onClose={() => setIsProfileDrawer(false)}
+      onClose={() => setIsProfileDrawerOpen(false)}
     >
-      {connected ? (
+      {mockConnected ? (
         <ProfileDrawerInnerContainer>
-          <MainNftProfileImage
-            src={NFTProfileSampleImage}
-            alt="main NFT profile"
-          />
+          <MainNftProfileImage src={currentNftProfile} alt="main NFT profile" />
           <div
-            style={{ display: "flex", margin: "30px 0", alignItems: "center" }}
+            style={{
+              display: "flex",
+              margin: "30px 0 15px 0",
+              alignItems: "center",
+            }}
           >
-            <Typography variant="h4" color="text.secondary">
+            <Typography variant="h5" color="text.primary">
               {name}
             </Typography>
             {isVerified && (
@@ -49,6 +58,59 @@ const ProfileDrawer = ({
               />
             )}
           </div>
+          <Typography
+            variant="body1"
+            color="text.primary"
+            sx={{ display: "flex", margin: "10px", alignItems: "center" }}
+          >
+            I am a lonely white wolf - interested in women
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.primary"
+            sx={{
+              display: "inline-block",
+              margin: "10px",
+              width: "500px",
+              textAlign: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              wordWrap: "break-word",
+            }}
+          >
+            Following &nbsp;
+            {followingHashtags.map((hashtag, index) => {
+              return (
+                <>
+                  {index <= 2 && (
+                    <>
+                      <a href="" style={{}}>
+                        #{hashtag}
+                      </a>
+                      , &nbsp;
+                    </>
+                  )}
+                </>
+              );
+            })}
+            {followingHashtags.length >= 3 && (
+              <a href="">+{followingHashtags.length - 3} MORE...</a>
+            )}
+          </Typography>
+          <OwnedNftImageList sx={{ width: 400 }} cols={3} rowHeight={400 / 3}>
+            {nftCollection.map((ownedNft, index) => {
+              return (
+                <ImageListItem key={index} sx={{ cursor: "pointer" }}>
+                  <img
+                    src={`${ownedNft}`}
+                    alt={ownedNft}
+                    loading="lazy"
+                    onClick={() => setCurrentNftProfile(ownedNft)}
+                  />
+                </ImageListItem>
+              );
+            })}
+          </OwnedNftImageList>
         </ProfileDrawerInnerContainer>
       ) : (
         <div style={{ margin: "auto" }}>Please Connect To Wallet</div>
